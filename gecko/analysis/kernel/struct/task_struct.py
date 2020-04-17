68207e3ebb4e2b3d2a6ec3316d9d0b34a9da4477
etbrem <etbrem2@gmail.com>
2020-04-17 16:39:47 +0300

    Update task_struct.py

--- gecko/analysis/kernel/struct/task_struct.py
+++ gecko/analysis/kernel/struct/task_struct.py
@@ -7,12 +7,14 @@ def comm(project=None):
     searchstr = b'"\x013Killed process %d (%s) total-vm:%'
     string_ea = project.ida.FindBinary(project.ida.MinEA(), project.ida.SEARCH_DOWN, searchstr)
 
+    # Find references to the string
     xrefs = list(project.ida.DataRefsTo(string_ea))
     assert len(xrefs) == 1
     xref = xrefs[0]
 
+    # Find the call after the reference (to printk or something)
     log_call_site = project.utils.get_next_callsite(xref)
 
+    # Symbolicly execute until the call (from start of func by default)
     wanted_regs = [2]
-
     found_state, found_offsets = project.utils.simulate_and_find_origins_simple(log_call_site, wanted_regs)
